d46968ba6c966b3b0d5b4f74bcbeee84716cde3a
Mark Uvari <muvari@gmail.com>
2020-03-25 14:58:37 -0400

    Server static files

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+// const Koa = require('koa');
 const Server = require('boardgame.io/server').Server;
-const LoveLetter = require('./LoveLetter').LoveLetter;
+const LoveLetter = require('./src/LoveLetter').LoveLetter;
 const server = Server({ games: [LoveLetter] });
+server.app.use(require('koa-static')('./build'));
 server.run(8000, () => console.log("server running..."));
--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './App.css';
 
-import { Client } from 'boardgame.io/react';
+// import { Client } from 'boardgame.io/react';
 // import { Local } from 'boardgame.io/multiplayer';
-import { SocketIO } from 'boardgame.io/multiplayer';
 import { Lobby } from 'boardgame.io/react';
 import { LoveLetter } from './LoveLetter';
 import LoveLetterBoard from './LoveLetterBoard';
 
-const LoveLetterClient = Client({ 
-  game: LoveLetter, 
-  board: LoveLetterBoard,
-  numPlayers: 2,
-  debug: true,
-  multiplayer: SocketIO({ server: 'http://localhost:8000/' }),
- });
+// const LoveLetterClient = Client({ 
+//   game: LoveLetter, 
+//   board: LoveLetterBoard, 
+//   numPlayers: 2,
+//   debug: true,
+//   multiplayer: Local(),
+//  });
 
  const App = () => (
   <div>
