569d0eea0f3af3adfd3d5b51ab88c61ff2aa962e
Mark Uvari <muvari@gmail.com>
2020-03-25 15:43:48 -0400

    Minor fixes

--- src/LoveLetter.js
+++ src/LoveLetter.js
@@ -180,7 +180,7 @@ export const LoveLetter = {
   endIf: (G, ctx) => {
     if (G.players.some((p) => p.wins === G.requiredWins)) {
       const winner = G.players.filter((p) => p.wins === G.requiredWins)[0];
-      return { winner: winner.id, message: `Player: ${winner.id} WINS!!!`}
+      return { winner: winner.id, message: `Player: ${G.gameMetadata[winner.id].name} WINS!!!`}
     }
   },
 };
