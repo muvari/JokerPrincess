e6c6a01daca305978f4028b02eac3b3819097b4f
Mark Uvari <muvari@gmail.com>
2020-03-25 15:05:08 -0400

    Specify heroku port

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const Server = require('boardgame.io/server').Server;
 const LoveLetter = require('./src/LoveLetter').LoveLetter;
 const server = Server({ games: [LoveLetter] });
 server.app.use(require('koa-static')('./build'));
-server.run(8000, () => console.log("server running..."));
+server.run(process.env.PORT || 8000, () => console.log("server running..."));
